95740fb1c5203c6eb579cd48c755780adf386147
Dinh Manh Tri <TriDMTH2008060@fpt.edu.vn>
2021-10-07 10:15:06 +0700

    update code

--- U42/MainPage.xaml.cs
+++ U42/MainPage.xaml.cs
@@ -1,10 +1,10 @@
-﻿using U41.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UserControlDataTemplate.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -15,9 +15,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace U41
+namespace UserControlDataTemplate
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -25,7 +25,7 @@ namespace U41
     public sealed partial class MainPage : Page
     {
         private List<Icon> Icons;
-        //private List<Contact> Contacts;
+        
         private ObservableCollection<Contact> Contacts;
 
         public MainPage()
@@ -40,8 +40,9 @@ namespace U41
             Icons.Add(new Icon { IconPath = "Assets/female-02.png" });
             Icons.Add(new Icon { IconPath = "Assets/female-03.png" });
 
-            //Contacts = new List<Contact>();
             Contacts = new ObservableCollection<Contact>();
+
+            //Contacts.Add(new Contact { FirstName = "Bob", LastName = "Tabor", AvatarPath = "Assets/male-03.png" });
         }
 
         private void NewContactButton_Click(object sender, RoutedEventArgs e)
